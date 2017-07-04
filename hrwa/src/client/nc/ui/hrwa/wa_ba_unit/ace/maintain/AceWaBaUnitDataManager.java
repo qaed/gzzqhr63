bddfae040d6d6b78aa57c922d932c2d13d0dbff7
tsy <1103277858@qq.com>
2017-07-04 09:17:43 +0800

    奖金分配单元初始化数据：按人力组织

--- hrwa/src/client/nc/ui/hrwa/wa_ba_unit/ace/maintain/AceWaBaUnitDataManager.java
+++ hrwa/src/client/nc/ui/hrwa/wa_ba_unit/ace/maintain/AceWaBaUnitDataManager.java
@@ -4,12 +4,11 @@ import nc.ui.pubapp.uif2app.model.BaseBillModelDataManager;
 import nc.vo.pubapp.AppContext;
 
 public class AceWaBaUnitDataManager extends BaseBillModelDataManager {
-
-	    @Override
-	    public void initModel() {
-		        String pk_group = AppContext.getInstance().getPkGroup();
-		        String sqlwhere = " and pk_group = '" + pk_group + "' ";
-		        super.initModelBySqlWhere(sqlwhere);
-	    }
-
+	@Override
+	public void initModel() {
+		String pk_group = AppContext.getInstance().getPkGroup();
+		String pk_org = getModel().getContext().getPk_org();
+		String sqlwhere = " and pk_group = '" + pk_group + "' and pk_org = '" + pk_org + "'";
+		super.initModelBySqlWhere(sqlwhere);
+	}
 }
