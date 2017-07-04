80430c3a5f378cf885a06115b7ae19a7b78e9250
tsy <1103277858@qq.com>
2017-07-04 09:20:58 +0800

    奖金分配单元：添加按钮默认分配单元类型为“自定义分配单元”

--- hrwa/src/client/nc/ui/hrwa/wa_ba_unit/ace/handler/AceAddHandler.java
+++ hrwa/src/client/nc/ui/hrwa/wa_ba_unit/ace/handler/AceAddHandler.java
@@ -5,6 +5,7 @@ import nc.ui.pub.bill.BillCardPanel;
 import nc.ui.pub.bill.BillItem;
 import nc.ui.pubapp.uif2app.event.IAppEventHandler;
 import nc.ui.pubapp.uif2app.event.billform.AddEvent;
+import nc.vo.pub.lang.UFDateTime;
 import nc.vo.pubapp.AppContext;
 import nc.vo.wa.wa_ba.unit.WaBaUnitHVO;
 
@@ -12,7 +13,15 @@ public class AceAddHandler implements IAppEventHandler<AddEvent> {
 	@Override
 	public void handleAppEvent(AddEvent e) {
 		BillCardPanel cardPanel = e.getBillForm().getBillCardPanel();
-		String pk_group = AppContext.getInstance().getPkGroup();
+		String pk_org = e.getContext().getPk_org();
+		AppContext appcontext = AppContext.getInstance();
+		String pk_group = appcontext.getPkGroup();
+		String pk_user = appcontext.getPkUser();
+		UFDateTime creationtime = appcontext.getServerTime();
+		cardPanel.setHeadItem("pk_org", pk_org);
 		cardPanel.setHeadItem("pk_group", pk_group);
+		cardPanel.setHeadItem("ba_unit_type", "自定义分配单元");
+		cardPanel.setTailItem("creator", pk_user);
+		cardPanel.setTailItem("creationtime", creationtime);
 	}
 }
