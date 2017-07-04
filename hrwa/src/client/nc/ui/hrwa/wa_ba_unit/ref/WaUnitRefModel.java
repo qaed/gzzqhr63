30218cacc383ebf3cb44c41b1066243e865c71dc
tsy <1103277858@qq.com>
2017-07-04 18:21:44 +0800

    新增奖金分配单元参照

---
+++ hrwa/src/client/nc/ui/hrwa/wa_ba_unit/ref/WaUnitRefModel.java
@@ -0,0 +1,21 @@
+package nc.ui.hrwa.wa_ba_unit.ref;
+
+import nc.ui.bd.ref.AbstractRefModel;
+
+public class WaUnitRefModel extends AbstractRefModel {
+	public WaUnitRefModel() {
+		reset();
+	}
+
+	public void reset() {
+		setRefNodeName("奖金分配单元");
+		setFieldCode(new String[] { "code", "name" });
+		setFieldName(new String[] { "奖金分配单元编码", "奖金分配单元名称" });
+		setHiddenFieldCode(new String[] { "pk_wa_ba_unit" });
+		setTableName("wa_ba_unit");
+		setPkFieldCode("pk_wa_ba_unit");
+		setDefaultFieldCount(2);
+		setRefTitle("奖金分配单元");
+		setWherePart(" isnull(dr,0)=0 and pk_org='" + this.getPk_org() + "'");
+	}
+}
