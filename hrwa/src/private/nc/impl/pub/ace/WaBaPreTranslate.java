4ebff09c5567665518967fc00065a8a40d09b3f5
tsy <1103277858@qq.com>
2017-07-20 10:07:00 +0800

    普通公式转化器

---
+++ hrwa/src/private/nc/impl/pub/ace/WaBaPreTranslate.java
@@ -0,0 +1,28 @@
+package nc.impl.pub.ace;
+
+import nc.impl.hr.formula.parser.IFormulaParser;
+import nc.vo.pub.BusinessException;
+
+public class WaBaPreTranslate implements IFormulaParser {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2297942556734015047L;
+
+	public String parse(String pk_org, String formula, Object... params) throws BusinessException {
+		// year,month,day,hourºÍsecond
+		String resultFormualr = "";
+		resultFormualr = formula.replace("\"", "'");
+		if (resultFormualr.contains("dateadd") || resultFormualr.contains("datediff")) {
+			resultFormualr = resultFormualr.replaceAll("'year'", "year");
+			resultFormualr = resultFormualr.replaceAll("'month'", "month");
+			resultFormualr = resultFormualr.replaceAll("'day'", "day");
+			resultFormualr = resultFormualr.replaceAll("'hour'", "hour");
+			resultFormualr = resultFormualr.replaceAll("'second'", "second");
+		}
+		return resultFormualr;
+
+	}
+
+}
