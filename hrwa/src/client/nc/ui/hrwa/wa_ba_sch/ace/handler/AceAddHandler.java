10f36936767df99180fcec813f2af3965fc8737e
tsy <1103277858@qq.com>
2017-07-28 17:50:09 +0800

    新增时，默认年份、期间、单据类型

--- hrwa/src/client/nc/ui/hrwa/wa_ba_sch/ace/handler/AceAddHandler.java
+++ hrwa/src/client/nc/ui/hrwa/wa_ba_sch/ace/handler/AceAddHandler.java
@@ -1,5 +1,7 @@
 package nc.ui.hrwa.wa_ba_sch.ace.handler;
 
+import java.util.Calendar;
+
 import nc.ui.pub.bill.BillCardPanel;
 import nc.ui.pubapp.uif2app.event.IAppEventHandler;
 import nc.ui.pubapp.uif2app.event.billform.AddEvent;
@@ -10,17 +12,21 @@ import nc.vo.pubapp.AppContext;
 
 public class AceAddHandler implements IAppEventHandler<AddEvent> {
 
-  @Override
-  public void handleAppEvent(AddEvent e) {
-    String pk_group = e.getContext().getPk_group();
-    String pk_org = e.getContext().getPk_org();
-    BillCardPanel panel = e.getBillForm().getBillCardPanel();
-	    //设置主组织默认值
-    panel.setHeadItem("pk_group", pk_group); 
-    panel.setHeadItem("pk_org", pk_org); 
-    //设置单据状态、日期默认值
-    panel.setHeadItem("approvestatus", BillStatusEnum.FREE.value());
-    panel.setHeadItem("billdate", AppContext.getInstance().getBusiDate()); 
-  }
+	@Override
+	public void handleAppEvent(AddEvent e) {
+		BillCardPanel panel = e.getBillForm().getBillCardPanel();
+		// 设置主组织默认值
+		panel.setHeadItem("pk_group", e.getContext().getPk_group());
+		panel.setHeadItem("pk_org", e.getContext().getPk_org());
+		// 设置单据状态、日期默认值
+		panel.setHeadItem("approvestatus", BillStatusEnum.FREE.value());
+		panel.setHeadItem("billdate", AppContext.getInstance().getBusiDate());
+		// 年度、带出期间
+		Calendar cal = Calendar.getInstance();
+		panel.setHeadItem("cyear", cal.get(Calendar.YEAR));
+		panel.setHeadItem("cperiod", cal.get(Calendar.MONTH));
+		// 单据类型
+		panel.setHeadItem("billtype", "BAAL");
+	}
 
 }
