ba708339e113adbf88eadf2d56aca4c83dd4a86b
tsy <1103277858@qq.com>
2017-08-02 10:20:30 +0800

    奖金项目点修改时，根据取值类型，禁用特定的输入框

--- hrwa/src/client/nc/ui/hrwa/wa_ba_item/ace/aciton/WaBaItemEditAction.java
+++ hrwa/src/client/nc/ui/hrwa/wa_ba_item/ace/aciton/WaBaItemEditAction.java
@@ -3,19 +3,49 @@ package nc.ui.hrwa.wa_ba_item.ace.aciton;
 import java.awt.event.ActionEvent;
 
 import nc.ui.pub.bill.BillCardPanel;
+import nc.ui.pubapp.uif2app.view.BillForm;
+import nc.ui.uif2.editor.IEditor;
+import nc.vo.wa.wa_ba.item.ItemsVO;
 
 public class WaBaItemEditAction extends nc.ui.pubapp.uif2app.actions.EditAction {
 
+	private IEditor editor;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8609030080700718241L;
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-//		BillCardPanel panel = e.getBillForm().getBillCardPanel();
 		super.actionPerformed(e);
+		ItemsVO vo = (ItemsVO) this.getModel().getSelectedData();
+		BillCardPanel panel = ((BillForm) editor).getBillCardPanel();
+		Integer datatype = vo.getDatatype();
+		panel.getHeadItem("value").setEnabled(false);
+		panel.getHeadItem("vformula").setEnabled(false);
+		panel.getHeadItem("vformulastr").setEnabled(false);
+		if (datatype == 0) {// 由公式计算
+			panel.getHeadItem("vformula").setEnabled(true);
+		} else if (datatype == 2) {// 手工输入
+		} else if (datatype == 3) {// 固定值
+			panel.getHeadItem("value").setEnabled(true);
+		} else if (datatype == 5) {// 其他数据源
+			panel.getHeadItem("vformula").setEnabled(true);
+		}
 	}
 
 	@Override
 	protected void beforeDoAction() {
-		//TODO tsy 组织状态下不能删除集团的
+		// TODO tsy 组织状态下不能删除集团的
 		super.beforeDoAction();
 	}
-	
+
+	public IEditor getEditor() {
+		return editor;
+	}
+
+	public void setEditor(IEditor editor) {
+		this.editor = editor;
+	}
+
 }
