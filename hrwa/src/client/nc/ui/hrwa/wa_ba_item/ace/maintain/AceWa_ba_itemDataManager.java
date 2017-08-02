137b2213be5460a977035329920f5ef4870f5a7a
tsy <1103277858@qq.com>
2017-08-02 16:20:09 +0800

    使用pk_org进行权限控制

--- hrwa/src/client/nc/ui/hrwa/wa_ba_item/ace/maintain/AceWa_ba_itemDataManager.java
+++ hrwa/src/client/nc/ui/hrwa/wa_ba_item/ace/maintain/AceWa_ba_itemDataManager.java
@@ -7,7 +7,6 @@ public class AceWa_ba_itemDataManager extends BaseBillModelDataManager {
 
 	@Override
 	public void initModel() {
-//		this.getModel().
 		String pk_org = getModel().getContext().getPk_org();
 //		String pk_group = AppContext.getInstance().getPkGroup();//0001A110000000000HED
 		String sqlwhere = " and pk_org = '" + pk_org + "' ";
