243bfde49f99185d57558f2da4611d1468a48a2c
tsy <1103277858@qq.com>
2017-09-25 14:39:32 +0800

    奖金分配方案及汇总的界面；单元保存校验表体不为空

--- hrwa/src/client/nc/ui/hrwa/wa_ba_unit/action/WaBaUnitSaveAction.java
+++ hrwa/src/client/nc/ui/hrwa/wa_ba_unit/action/WaBaUnitSaveAction.java
@@ -1,12 +1,21 @@
 package nc.ui.hrwa.wa_ba_unit.action;
 
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.List;
 
+import nc.bs.uif2.BusinessExceptionAdapter;
+import nc.bs.uif2.validation.ValidationException;
+import nc.bs.uif2.validation.ValidationFailure;
 import nc.ui.ml.NCLangRes;
 import nc.ui.pub.beans.MessageDialog;
 import nc.ui.pub.beans.UIDialog;
 import nc.ui.pubapp.uif2app.actions.SaveAction;
-import nc.ui.uif2.components.CommonConfirmDialogUtils;
+import nc.ui.pubapp.uif2app.view.BillForm;
+import nc.ui.uif2.ShowStatusBarMsgUtil;
+import nc.ui.uif2.UIState;
+import nc.vo.ml.NCLangRes4VoTransl;
+import nc.vo.wa.wa_ba.unit.AggWaBaUnitHVO;
 
 public class WaBaUnitSaveAction extends SaveAction {
 
@@ -21,12 +30,30 @@ public class WaBaUnitSaveAction extends SaveAction {
 	@Override
 	public void doAction(ActionEvent e) throws Exception {
 		//		if (UIDialog.ID_YES == CommonConfirmDialogUtils.showConfirmSaveDialog(getModel().getContext().getEntranceUI())) {
-		String title = NCLangRes.getInstance().getStrByID("uif2", "CommonConfirmDialogUtils-000000")/*确认保存*/;
 		//		String QUESTION = NCLangRes.getInstance().getStrByID("uif2", "CommonConfirmDialogUtils-000001")/*是否要保存已修改的数据？*/;
-		String question = "确定保存编辑数据?";
-		if (UIDialog.ID_YES == MessageDialog.showYesNoDlg(getModel().getContext().getEntranceUI(), title, question, UIDialog.ID_NO)) {
-			super.doAction(e);
+		if (getModel().getUiState() != UIState.ADD) {
+			String title = NCLangRes.getInstance().getStrByID("uif2", "CommonConfirmDialogUtils-000000")/*确认保存*/;
+			String question = "确定保存编辑数据?";
+			if (UIDialog.ID_YES != MessageDialog.showYesNoDlg(getModel().getContext().getEntranceUI(), title, question, UIDialog.ID_NO)) {
+				return;
+			}
 		}
+		super.doAction(e);
+
+	}
+
+	/* （非 Javadoc）
+	 * @see nc.ui.uif2.actions.SaveAction#validate(java.lang.Object)
+	 */
+	@Override
+	protected void validate(Object value) {
+		AggWaBaUnitHVO aggvo = (AggWaBaUnitHVO) ((BillForm) getEditor()).getModel().getSelectedData();
+		if (aggvo.getChildrenVO().length < 1) {
+			List<ValidationFailure> list = new ArrayList();
+			list.add(new ValidationFailure("表体不能为空"));
+			throw new BusinessExceptionAdapter(new ValidationException(list));
+		}
+		super.validate(value);
 	}
 
 }
