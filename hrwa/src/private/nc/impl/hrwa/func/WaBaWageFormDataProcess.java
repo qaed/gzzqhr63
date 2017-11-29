1bd21f043b45bb5e1f8b02cedcc4008f882edd86
tsy <1103277858@qq.com>
2017-11-29 15:01:36 +0800

    优化分配计算时取最新工作记录的sql

--- hrwa/src/private/nc/impl/hrwa/func/WaBaWageFormDataProcess.java
+++ hrwa/src/private/nc/impl/hrwa/func/WaBaWageFormDataProcess.java
@@ -1,25 +1,13 @@
 package nc.impl.hrwa.func;
 
 import java.math.BigDecimal;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.lang.StringUtils;
-
-import uap.lfw.bd.reference.IReferenceQry.simpleRefVO;
-
-import nc.hr.utils.SQLHelper;
-import nc.jdbc.framework.processor.BeanProcessor;
-import nc.jdbc.framework.processor.ColumnProcessor;
 import nc.jdbc.framework.processor.MapListProcessor;
 import nc.vo.pub.BusinessException;
-import nc.vo.pub.ISuperVO;
-import nc.vo.pub.lang.UFLiteralDate;
-import nc.vo.wa.rule.WageformdetVO;
-import nc.vo.wa.wa_ba.sch.WaBaSchBVO;
-import nc.vo.wa.wa_ba.sch.WaBaSchHVO;
+
+import org.apache.commons.lang.StringUtils;
 
 /**
  * 薪资规则表
@@ -28,6 +16,9 @@ import nc.vo.wa.wa_ba.sch.WaBaSchHVO;
  */
 public class WaBaWageFormDataProcess extends WaBaCommonProcess {
 
+	private static final long serialVersionUID = -4417686855450280940L;
+
+	@SuppressWarnings("unchecked")
 	public String parse(String pk_org, String functionName, Object... params) throws BusinessException {
 		/*
 		 * wageFormData(1001A11000000000A48N)
@@ -48,11 +39,20 @@ public class WaBaWageFormDataProcess extends WaBaCommonProcess {
 			sqlB.append(nvalue);
 		}
 		sqlB.append(" else 0 end ");
+		sqlB.append(" from hi_psnjob left join bd_psndoc on bd_psndoc.pk_psndoc = hi_psnjob.pk_psndoc");
+		sqlB.append(" left join hi_psnorg on hi_psnorg.pk_psnorg = hi_psnjob.pk_psnorg");
+		sqlB.append(" where wa_ba_sch_psns.pk_psndoc=hi_psnjob.pk_psndoc ");
+		sqlB.append(" and hi_psnjob.lastflag='Y' ");
+		sqlB.append(" and hi_psnjob.ismainjob='Y' ");
+		sqlB.append(" and hi_psnorg.lastflag='Y' ");
+		/*
+		sqlB.append(" else 0 end ");
 		sqlB.append(" from hi_psnjob,bd_psndoc ");
 		sqlB.append(" where wa_ba_sch_psns.pk_psndoc=hi_psnjob.pk_psndoc ");
 		sqlB.append(" and wa_ba_sch_psns.pk_psndoc=bd_psndoc.pk_psndoc ");
 		sqlB.append(" and hi_psnjob.lastflag='Y' ");
 		sqlB.append(" and hi_psnjob.ismainjob='Y' ");
+		 */
 		/*
 		 * select case 
 		 * 		when(hi_psnjob.pk_org = '0001A110000000000HED') then 1.20000000
@@ -72,7 +72,7 @@ public class WaBaWageFormDataProcess extends WaBaCommonProcess {
 	 * @throws BusinessException
 	 */
 	private String parseFormula(String formula) throws BusinessException {
-		String[] parameter = null;
+		//		String[] parameter = null;
 		/*
 		 * 
 		 */
