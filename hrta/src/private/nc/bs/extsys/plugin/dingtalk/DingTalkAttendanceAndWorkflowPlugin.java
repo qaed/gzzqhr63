e3f78e85ec02d51533a20031a0c775c55746dc92
tsy <1103277858@qq.com>
2017-12-26 10:36:59 +0800

    钉钉修改初次同步为同步人员至HR

---
+++ hrta/src/private/nc/bs/extsys/plugin/dingtalk/DingTalkAttendanceAndWorkflowPlugin.java
@@ -0,0 +1,92 @@
+package nc.bs.extsys.plugin.dingtalk;
+
+import nc.bs.dao.BaseDAO;
+import nc.bs.extsys.plugin.dingtalk.attendance.SyncAttendance;
+import nc.bs.extsys.plugin.dingtalk.user.SyncUser;
+import nc.bs.extsys.plugin.dingtalk.workflow.SyncWorkFlow;
+import nc.bs.logging.Logger;
+import nc.bs.pub.pa.PreAlertObject;
+import nc.bs.pub.pa.PreAlertReturnType;
+import nc.bs.pub.taskcenter.BgWorkingContext;
+import nc.bs.pub.taskcenter.IBackgroundWorkPlugin;
+import nc.vo.pub.BusinessException;
+
+/**
+ * 阿里钉钉考勤签到审批同步到NC
+ * 
+ * @author tsheay
+ */
+public class DingTalkAttendanceAndWorkflowPlugin implements IBackgroundWorkPlugin {
+
+	@Override
+	public PreAlertObject executeTask(BgWorkingContext arg0) throws BusinessException {
+		int attendanceErrorTimes = 0;
+		int workflowErrorTimes = 0;
+		int attendanceErrorMaxTimes = 20;
+		int workflowErrorMaxTimes = 20;
+
+		PreAlertObject alert = new PreAlertObject();
+		alert.setMsgTitle("同步钉钉任务执行情况");
+		StringBuilder returnmsg = new StringBuilder();
+		BaseDAO dao = new BaseDAO();
+		//		SyncDept syncdept = new SyncDept(dao);
+		SyncAttendance syncAttendance = new SyncAttendance(dao);
+		SyncWorkFlow syncWorkFlow = new SyncWorkFlow(dao);
+		PreAlertObject attendanceAlert = null;
+		PreAlertObject workFlowAlert = null;
+		//同步考勤、签到
+		try {
+			attendanceAlert = syncAttendance.executeTask(arg0);
+		} catch (Exception e) {
+			if (attendanceErrorTimes++ < attendanceErrorMaxTimes) {
+				if (e.getMessage().contains("-1") || e.getMessage().contains("-2")) {
+					try {
+						Thread.sleep(3000L);
+					} catch (InterruptedException e1) {
+						Logger.error(e1);
+					}
+					attendanceAlert = syncAttendance.executeTask(arg0);
+				} else {
+					throw new BusinessException(e);
+				}
+			} else {
+				returnmsg.append("====考勤、签到同步失败，网络连接超时，请稍后再试试\n");
+			}
+		}
+		//同步出差、外出
+		try {
+			workFlowAlert = syncWorkFlow.executeTask(arg0);
+		} catch (Exception e) {
+			if (workflowErrorTimes++ < workflowErrorMaxTimes) {
+				if (e.getMessage().contains("-1") || e.getMessage().contains("-2")) {
+					try {
+						Thread.sleep(3000L);
+					} catch (InterruptedException e1) {
+						Logger.error(e1);
+					}
+					workFlowAlert = syncWorkFlow.executeTask(arg0);
+				} else {
+					throw new BusinessException(e);
+				}
+			} else {
+				returnmsg.append("====外出、出差同步失败，网络连接超时，请稍后再试试\n");
+			}
+		}
+		if (attendanceAlert != null && attendanceAlert.getReturnObj() != null) {
+			returnmsg.append((String) attendanceAlert.getReturnObj());
+			returnmsg.append("尝试同步考勤次数:" + (attendanceErrorTimes + 1) + "\n\n");
+		} else {
+			returnmsg.append("同步考勤失败！\n\n");
+		}
+		if (workFlowAlert != null && workFlowAlert.getReturnObj() != null) {
+			returnmsg.append((String) workFlowAlert.getReturnObj());
+			returnmsg.append("尝试同步出差审批次数:" + (attendanceErrorTimes + 1) + "\n\n");
+		} else {
+			returnmsg.append("同步出差审批失败！\n\n");
+		}
+		returnmsg.append("===============同步任务完成===============\n");
+		alert.setReturnObj(returnmsg.toString());
+		alert.setReturnType(PreAlertReturnType.RETURNMESSAGE);
+		return alert;
+	}
+}
--- hrta/src/private/nc/bs/extsys/plugin/dingtalk/user/SyncUserToHR.java
+++ hrta/src/private/nc/bs/extsys/plugin/dingtalk/user/SyncUserToHR.java
@@ -18,22 +18,27 @@ import nc.bs.pub.pa.PreAlertObject;
 import nc.bs.pub.pa.PreAlertReturnType;
 import nc.bs.pub.taskcenter.BgWorkingContext;
 import nc.bs.pub.taskcenter.IBackgroundWorkPlugin;
-import nc.impl.pubapp.pattern.data.vo.VOInsert;
+import nc.bs.uif2.validation.ValidationException;
+import nc.impl.ta.psndoc.TBMPsndocDAO;
+import nc.itf.ta.IPeriodQueryMaintain;
 import nc.itf.ta.TBMPsndocDelegator;
-import nc.jdbc.framework.SQLParameter;
+import nc.jdbc.framework.processor.ColumnProcessor;
 import nc.jdbc.framework.processor.MapProcessor;
-import nc.ui.ta.psndoc.model.TbmPsndocAppModelService;
 import nc.vo.pub.BusinessException;
 import nc.vo.pub.lang.UFDateTime;
 import nc.vo.pub.lang.UFLiteralDate;
 import nc.vo.ta.psndoc.TBMPsndocVO;
+
+import org.apache.commons.lang.StringUtils;
 
 import com.dingtalk.open.client.api.model.corp.CorpUser;
 import com.dingtalk.open.client.api.model.corp.CorpUserDetail;
 import com.dingtalk.open.client.api.model.corp.CorpUserList;
 import com.dingtalk.open.client.api.model.corp.Department;
 
+@SuppressWarnings("restriction")
 public class SyncUserToHR implements IBackgroundWorkPlugin {
+	IPeriodQueryMaintain periodmaintain = NCLocator.getInstance().lookup(IPeriodQueryMaintain.class);
 	public SyncUserToHR() {
 
 	}
@@ -53,91 +58,38 @@ public class SyncUserToHR implements IBackgroundWorkPlugin {
 	 */
 	public PreAlertObject executeTask(BgWorkingContext arg0) throws BusinessException {
 		PreAlertObject alert = new PreAlertObject();
-		alert.setReturnType(PreAlertReturnType.RETURNMESSAGE);
+		alert.setMsgTitle("同步人员任务执行情况22223");
 		StringBuilder returnmsg = new StringBuilder();
-		alert.setMsgTitle("人员同步任务执行情况（钉钉-->HR）");
 		StringBuilder sql = new StringBuilder();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:dd");
+		//即将保存的考勤档案
 		List<TBMPsndocVO> insertvos = new ArrayList<TBMPsndocVO>();
-		List<String> insertpsndocs = new ArrayList<String>();
-		/*
-		//分布try包围，方便记录错误信息
-		List<Department> departs = null;
-		//获取全部部门
-		try {
-			departs = DepartmentHelper.listDepartments(AuthHelper.getAccessToken(), "1");
-		} catch (Exception e) {
-			Logger.error(e);
-			throw new BusinessException("同步失败，获取部门信息失败，请稍后再试", e);
-		}
-
-		for (Department department : departs) {
-			CorpUserList corpUserList = null;
-			try {//获取部门下的所有人员
-				corpUserList = UserHelper.getDepartmentUser(AuthHelper.getAccessToken(), department.getId(), null, null, null);
-			} catch (Exception e) {
-				Logger.error(e);
-				returnmsg.append("获取部门成员失败，请稍后再试__部门ID:" + department.getId() + ",部门名称:" + department.getName() + "\n");
-			}
-			if (corpUserList == null) {
-				continue;
-			}
-			List<CorpUser> userList = corpUserList.getUserlist();
-			for (int i = 0; userList != null && i < userList.size(); i++) {
-				CorpUser user = userList.get(i);
-				CorpUserDetail userDetail = null;
-				try {//获取人员详细信息
-					userDetail = UserHelper.getUser(getToken(), user.getUserid());
-				} catch (Exception e) {
-					Logger.error(e);
-					returnmsg.append("获取人员详细信息失败，请稍后再试__部门名称:" + department.getName() + ",人员名称:" + user.getName() + "\n");
-				}
-				if (userDetail == null) {
-					continue;
-				}
-				String mobile = userDetail.getMobile();
-				sql.delete(0, sql.length());
-				sql.append("select b.begindate,b.pk_group,b.pk_org,a.pk_psndoc,b.pk_psnjob,b.pk_psnorg from bd_psndoc a left join hi_psnjob b on a.pk_psndoc = b.pk_psndoc  where a.mobile='" + mobile + "' and b.lastflag='Y'");
-				Map<String, String> result = (Map<String, String>) getDao().executeQuery(sql.toString(), new MapProcessor());
-				if (result == null) {
-					returnmsg.append("未能找到相应的手机号码，请在HR系统维护手机号码__钉钉人员:" + userDetail.getName() + ",手机号:" + userDetail.getMobile() + "\n");
-					continue;
-				}
-				//保存待插入数据
-				TBMPsndocVO vo = new TBMPsndocVO();
-				vo.setBegindate(new UFLiteralDate("2017-08-01"));//考勤开始时间result.get("begindate")
-				vo.setCreationtime(new UFDateTime(Calendar.getInstance().getTime()));//创建时间
-				vo.setPk_psndoc(result.get("pk_psndoc"));//默认他自己
-				vo.setPk_group(result.get("pk_group"));//集团
-				vo.setPk_org(result.get("pk_org"));//组织
-				vo.setPk_psndoc(result.get("pk_psndoc"));//人员主键
-				vo.setPk_psnjob(result.get("pk_psnjob"));//工作主键
-				vo.setPk_psnorg(result.get("pk_psnorg"));//组织关系
-				vo.setEnddate(new UFLiteralDate("9999-12-01"));//结束日期
-				vo.setTbm_prop(2);//考勤方式:1=手工考勤，2=机器考勤
-				vo.setTimecardid(userDetail.getUserid());//考勤卡号
-				insertvos.add(vo);
-			}
-		}
-		*/
+		//即将更新考勤卡号的考勤档案
+		List<TBMPsndocVO> updatevos = new ArrayList<TBMPsndocVO>();
+		//已插入的pk_psndoc
+		//		List<String> insertpsndocs = new ArrayList<String>();
+		Set<String> insertpk_psndoc = new HashSet<String>();
+		TBMPsndocDAO tbmdao = new TBMPsndocDAO();
 
 		List<CorpUserDetail> userList = getAllUser();
+		returnmsg.append("以下未能找到相应的手机号码，请在HR系统维护手机号码:" );
 		for (CorpUserDetail userDetail : userList) {
 			String mobile = userDetail.getMobile();
 			sql.delete(0, sql.length());
 			sql.append("select b.begindate,b.pk_group,b.pk_org,a.pk_psndoc,b.pk_psnjob,b.pk_psnorg from bd_psndoc a left join hi_psnjob b on a.pk_psndoc = b.pk_psndoc  where a.mobile='" + mobile + "' and b.lastflag='Y'");
 			Map<String, String> result = (Map<String, String>) getDao().executeQuery(sql.toString(), new MapProcessor());
 			if (result == null) {
-				returnmsg.append("未能找到相应的手机号码，请在HR系统维护手机号码__钉钉人员:" + userDetail.getName() + ",手机号:" + userDetail.getMobile() + "\n");
+				returnmsg.append(userDetail.getName() + ",手机号:" + userDetail.getMobile() + ";");
 				continue;
 			}
 			//过滤重复人员
-			if (insertpsndocs.contains(result.get("pk_psndoc"))) {
+			if (insertpk_psndoc.contains(result.get("pk_psndoc"))) {
 				continue;
 			}
 			//保存待插入数据
 			TBMPsndocVO vo = new TBMPsndocVO();
-			vo.setBegindate(new UFLiteralDate("2017-08-01"));//考勤开始时间result.get("begindate")
+			String begindate = (String) getDao().executeQuery("select min(timeyear||'-'||timemonth||'-01') from tbm_period where sealflag='N'", new ColumnProcessor());
+			vo.setBegindate(new UFLiteralDate(begindate));//考勤开始时间result.get("begindate")
 			vo.setCreationtime(new UFDateTime(Calendar.getInstance().getTime()));//创建时间
 			vo.setPk_psndoc(result.get("pk_psndoc"));//默认他自己
 			vo.setPk_group(result.get("pk_group"));//集团
@@ -149,16 +101,40 @@ public class SyncUserToHR implements IBackgroundWorkPlugin {
 			vo.setTbm_prop(2);//考勤方式:1=手工考勤，2=机器考勤
 			vo.setTimecardid(userDetail.getUserid());//考勤卡号
 			vo.setPk_adminorg(result.get("pk_org"));//管理组织
-			insertpsndocs.add(result.get("pk_psndoc"));//保存到一个list，方便去重
+			//			insertpsndocs.add(result.get("pk_psndoc"));
+
+			insertpk_psndoc.add(result.get("pk_psndoc"));//保存到一个set，方便去重
 			insertvos.add(vo);
+			try {
+				TBMPsndocDelegator.getTBMPsndocManageMaintain().check(vo);
+			} catch (BusinessException e) {
+				if (e instanceof ValidationException && e.getMessage() != null && e.getMessage().contains("未结束的考勤档案")) {
+					//报错为：XXX已经有未结束的考勤档案！
+					TBMPsndocVO tbmvo =
+							tbmdao.queryLatestByPsndocDate(vo.getPk_org(), vo.getPk_psndoc(), vo.getBegindate(), new UFLiteralDate());
+					//更新tbmvo的考勤卡号
+					if (!StringUtils.equals(tbmvo.getTimecardid(), userDetail.getUserid())) {
+						tbmvo.setTimecardid(userDetail.getUserid());
+						updatevos.add(tbmvo);
+					}
+				} else {
+					returnmsg.append(e.getMessage()+"\n");
+				}
+				insertpk_psndoc.remove(result.get("pk_psndoc"));
+				insertvos.remove(vo);
+
+			}
 		}
 		//		VOInsert<TBMPsndocVO> voInsert = new VOInsert<TBMPsndocVO>();
 		//执行保存
-		TBMPsndocDelegator.getTBMPsndocManageMaintain().check(insertvos.toArray(new TBMPsndocVO[0]));
+//		TBMPsndocDelegator.getTBMPsndocManageMaintain().check(insertvos.toArray(new TBMPsndocVO[0]));
 		TBMPsndocDelegator.getTBMPsndocManageMaintain().insert(insertvos.toArray(new TBMPsndocVO[0]), true);
+		getDao().updateVOArray(updatevos.toArray(new TBMPsndocVO[0]), new String[] { TBMPsndocVO.TIMECARDID });
 		//		voInsert.insert(insertvos.toArray(new TBMPsndocVO[0]));
 		returnmsg.append("同步完成\n");
 		alert.setReturnObj(returnmsg.toString());
+		alert.setReturnType(PreAlertReturnType.RETURNMESSAGE);
+
 
 		return alert;
 	}
